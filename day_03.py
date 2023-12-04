ad202ace8c9e693db8d2c0a6e1f5966d59a14931
Jared Welch <jared@jeli.io>
2023-12-04 09:52:25 -0800

    day 3 part 2 done late

--- day_03.py
+++ day_03.py
@@ -1,6 +1,7 @@
+from collections import defaultdict
 from pprint import pprint
 
-from lib import run, build_grid, neighbors_fn
+from lib import run, build_grid, neighbors_fn, Point
 from typing import Any
 
 
@@ -22,64 +23,88 @@ def solve_one(data: str) -> Any:
     # ...$.*....
     # .664.598.."""
 
+    # g, h, w, points, values = build_grid(data, split_fn=lambda x: list(x))
+    # print(g[0])
+    # print(f"{h=} {w=}")
+    # # pprint(points)
+    # part_nos = []
+    # nfn = neighbors_fn(h, w, True)
+    # nums = set("1234567890")
+    #
+    # total = 0
+    #
+    # for line_idx, line in enumerate(g):
+    #     for col_idx, col in enumerate(line):
+    #         if (
+    #             points[col_idx, line_idx] in nums
+    #             and not points.get((col_idx - 1, line_idx), ".") in nums
+    #         ):
+    #             x2 = col_idx
+    #             t = ""
+    #             part_no = False
+    #             while points.get((x2, line_idx), ".") in nums:
+    #                 t += points[x2, line_idx]
+    #                 for nx, ny in nfn((x2, line_idx)):
+    #                     if not points[nx, ny] in {".", "0"} | nums:
+    #                         part_no = True
+    #
+    #                 x2 += 1
+    #             if part_no:
+    #                 total += int(t)
+
+
+    # assert total == 4361, f"Total is {total}"
+    # return total
+    pass
+
+def solve_two(data: str) -> Any:
+#     data = """467..114..
+# ...*......
+# ..35..633.
+# ......#...
+# 617*.......
+# .....+.58.
+# ..592.....
+# ......755.
+# ...$.*....
+# .664.598.."""
+
     g, h, w, points, values = build_grid(data, split_fn=lambda x: list(x))
     print(g[0])
     print(f"{h=} {w=}")
-    # pprint(points)
-    part_nos = []
     nfn = neighbors_fn(h, w, True)
     nums = set("1234567890")
 
     total = 0
-
+    gears: dict[Point, list[str]] = defaultdict(list)
     for line_idx, line in enumerate(g):
         for col_idx, col in enumerate(line):
             if (
-                points[col_idx, line_idx] in nums
-                and not points.get((col_idx - 1, line_idx), ".") in nums
+                    points[col_idx, line_idx] in nums
+                    and not points.get((col_idx - 1, line_idx), ".") in nums
             ):
                 x2 = col_idx
                 t = ""
-                part_no = False
+                gear = None
                 while points.get((x2, line_idx), ".") in nums:
                     t += points[x2, line_idx]
                     for nx, ny in nfn((x2, line_idx)):
-                        if not points[nx, ny] in {".", "0"} | nums:
-                            part_no = True
+                        checking = points[nx, ny]
+                        print(f"checking ({nx}, {ny}) = {checking} from ({x2, line_idx}) = {points[x2, line_idx]}")
+                        if checking == "*":
+                            gear = (nx, ny)
+
                     x2 += 1
-                if part_no:
-                    total += int(t)
+                if gear is not None:
+                    gears[gear].append(t)
+    for gear, parts in gears.items():
+        if len(parts) != 2:
+            continue
 
-            # print(cur_num)
-            # if col.isdigit():
-            #     cur_num += col
-            #     # find neighbors
-            #
-            #     if is_part_no:
-            #         continue
-            #
-            #     print(f"{col} is a number")
-            #     neighbors = nfn((col_idx, line_idx))
-            #     for (nx, ny) in neighbors:
-            #         print(f"neighbor {nx=} {ny=} is {g[ny][nx]}")
-            #         p = g[ny][nx]
-            #         if not p.isdigit() and p != ".":
-            #             is_part_no = True
-            #             break
-            #
-            # else:
-            #     if is_part_no:
-            #         part_nos.append(cur_num)
-            #         is_part_no = False
-            #
-            #     cur_num = ""
+        total += int(parts[0]) * int(parts[1])
 
-    assert total == 4361, f"Total is {total}"
+    assert total == 467835, f"Total is {total}"
     return total
-
-
-def solve_two(data: str) -> Any:
-    pass
 
 
 def main(quiet: bool = False) -> None:
